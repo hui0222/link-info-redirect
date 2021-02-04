c12f58740c5dc4fa55a2902158773bb81645aa33
hui <hui02222@naver.com>
2021-02-04 23:40:05 +0900

    springboot - mustache 설정 및 메인페이지 생성

--- src/main/java/kr/co/lir/controller/MainController.java
+++ src/main/java/kr/co/lir/controller/MainController.java
@@ -1,14 +1,13 @@
 package kr.co.lir.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
-    @GetMapping(value = "/main/test")
-    public String test(){
-        return "test";
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
     }
 }
