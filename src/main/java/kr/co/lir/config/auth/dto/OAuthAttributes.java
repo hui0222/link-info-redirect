21cad9a2a366ef19934c64adb605432f22cab5d3
hui <hui02222@naver.com>
2021-02-17 23:55:39 +0900

    login kakao add

--- src/main/java/kr/co/lir/config/auth/dto/OAuthAttributes.java
+++ src/main/java/kr/co/lir/config/auth/dto/OAuthAttributes.java
@@ -37,6 +37,8 @@ public class OAuthAttributes {
     ) {
         if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
+        } else if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
         }
 
         return ofGoogle(userNameAttributeName, attributes);
@@ -58,6 +60,18 @@ public class OAuthAttributes {
         return  OAuthAttributes.builder()
                 .name((String) response.get("name"))
                 .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String,Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return  OAuthAttributes.builder()
+                .name((String) response.get("nickname"))
+                .email((String) response.get("account_email"))
                 .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
